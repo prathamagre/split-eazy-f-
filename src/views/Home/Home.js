227863f1462cbef27fa9237f8b45d056ac814a7c
prathamagre <prathamvaishu@gmail.com>
2024-11-16 13:47:30 +0530

    home page upper content added

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import {useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import "./../Home/Home.css"
 import Navabar from "./../../components/navbar/Navbar";
+import Homeimg from "./../../assests/home-img.webp"
 
 function Home() {
   const navigate = useNavigate();
   return (
     <div>
-      <Navabar/>
-      <div>
-        <div>
-        <img></img>
-        </div>
-        <div>
-          <h2>Spliteasy</h2>
-          <p>Manage Group Expenses Without the Math Headache!</p>
-        <button onClick={()=>navigate("/add-list")} className="get-started-btn">Get Started</button>
-        </div>
+      <Navabar />
+      <div className='top-content'>
 
-      
+        <img className="home-img" src={Homeimg}></img>
+
+        <div className='top-cont'>
+          <h2 className='top-cont-txt'>Manage group expenses without the math headache with <span className='cont-name'>SplitEasy</span></h2>
+          <button onClick={() => navigate("/add-list")} className="get-started-btn">Get Started</button>
+        </div>
       </div>
     </div>
   )
