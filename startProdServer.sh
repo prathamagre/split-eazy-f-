738ba3b37952c8f3a97c9cb198c11a2ee382a56e
prathamagre <prathamvaishu@gmail.com>
2025-10-03 19:50:33 +0530

    changes added

--- startProdServer.sh
+++
@@ -1,1 +0,0 @@
-mypython3 -m gunicorn -w 2 -b "127.0.0.1:5000" -k sync "sebackend:create_app()"
--- testing/clearAllData.sh
+++
@@ -1,4 +0,0 @@
-rm ./data/listOfListings.json
-rm ./data/payments.json
-cp testing/listOfListings_initial.json data/listOfListings.json
-cp testing/payments_initial.json data/payments.json
